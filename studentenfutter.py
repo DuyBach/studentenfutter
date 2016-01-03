81d3f6803cfd1fd67355331464fe4872fdfb01c7
Duy <duy@scondoo.de>
2016-01-03 15:27:17 +0100

    Add user and foodshop model

--- studentenfutter.py
+++ studentenfutter.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)
+db.init_app(app)
 
 
 @app.route('/')
