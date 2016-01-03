5e5f0b05922f7d4bd66a17facd78b80aa55d5bf9
Duy <duy@scondoo.de>
2016-01-03 19:07:49 +0100

    Extend models

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
 
@@ -9,6 +10,25 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     admin = db.Column(db.Boolean)
+    active = db.Column(db.Boolean, default=True)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id.decode()
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class Foodshop(db.Model):
--- studentenfutter.py
+++ studentenfutter.py
@@ -1,14 +1,39 @@
 from flask import Flask, render_template
-from models import db
+from flask_login import LoginManager
+from models import db, User
 
 app = Flask(__name__)
+
 app.config.from_pyfile('config.py')
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    pass
+
+
+@app.route('/login')
+def login():
+    pass
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+def load_user(user_id):
+    return User.get(user_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
