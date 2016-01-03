e65e9d78124453c41d047a008eeab38e0bdf9b58
Duy <duy@scondoo.de>
2016-01-03 15:44:11 +0100

    Extend models

--- models.py
+++ models.py
@@ -5,10 +5,12 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    admin = db.Column(db.Boolean)
 
 
 class Foodshop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
