97ba858a75e856d34cc69481553010c608597b6a
Duy <duy@scondoo.de>
2016-01-06 18:25:31 +0100

    Init new project structure

---
+++ app/forms.py
@@ -0,0 +1,27 @@
+from wtforms import Form, validators, StringField, PasswordField, SubmitField
+from app import db, models
+
+
+class SignupForm(Form):
+    username = StringField('Username', [validators.required(), validators.Length(min=3, max=30)])
+    password = PasswordField('Password', [validators.required(), validators.Length(min=6, max=30)])
+    email = StringField('Email', [validators.required(), validators.Length(min=6, max=35), validators.Email()])
+    submit = SubmitField("Create account")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self, *args, **kwargs)
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        username = models.User.query.filter_by(username=self.username.data.lower()).first()
+        email = models.User.query.filter_by(email=self.email.data.lower()).first()
+        if username:
+            self.username.errors.append("That username is already taken")
+            return False
+        elif email:
+            self.email.errors.append("That email is already taken")
+            return False
+        else:
+            return True
---
+++ app/views.py
@@ -0,0 +1,26 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+from .forms import SignupForm
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm(request.values)
+    
+    if request.method == 'POST':
+        if form.validate():
+            # CREATE
+            # SIGN IN
+            # REDIRECT
+            # user = User(username=form.username.data, password=form.password.data, email=form.email.data)
+            # app.logger.info(user)
+            return redirect(url_for('index'))
+        else:
+            return render_template('signup.html', form=form)
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
