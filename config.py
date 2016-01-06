282635608bcd3f56552dc2d50661f2d9207500eb
Duy <duy@scondoo.de>
2016-01-06 18:24:18 +0100

    Init new project structure

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:studentenfutter@localhost/studentenfutter'
+import os
+
+
+WTF_CSRF_ENABLE = True
 SECRET_KEY = '\xa4\x93Q\xf1q2\x02J\xec;\xdd\xf7\xe3(\xb6\x92;\xbd\x8e\xdf\xd3\x10N\x03'
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:studentenfutter@localhost/studentenfutter'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
